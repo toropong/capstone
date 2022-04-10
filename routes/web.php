0c52fb472bff16a33a954fcabdc7b318b99846d3
kmkm37 <60097820+kmkm37@users.noreply.github.com>
2022-04-11 00:38:05 +0900

    정경진 04-11 템플릿 적용

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/main', function () {
+    return view('main');
 });
