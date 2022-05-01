11124572303464d852e6eaa2ffcef20b6926627b
ag9zda <lhoswj@gmail.com>
2022-05-01 11:49:19 +0900

    석상일 유저 비밀번호 변경 기능

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-/**
- * @property User $user
- */
 class UserController extends Controller
 {
     public function showPasswordChangeForm(Request $request)
