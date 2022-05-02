d8f5f681c1d576d153932cc269c7edc9e8c3d874
ag9zda <lhoswj@gmail.com>
2022-05-02 22:15:59 +0900

    석상일 네비바 index, main 연결

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container px-5">
-        <a class="navbar-brand" href="{{ route('main') }}">중부대학교 졸업 작품</a>
+        <a class="navbar-brand" href="{{ route('index') }}">중부대학교 졸업 작품</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="{{ route('main') }}">홈</a></li>
-                <li class="nav-item"><a class="nav-link" href="intro.php">행사 소개</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('index') }}">홈</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('main') }}">행사 소개</a></li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">작품 리스트</a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
